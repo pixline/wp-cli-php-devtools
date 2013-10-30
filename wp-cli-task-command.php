12ba35b94f40aef8d8054bb88ad9bc061154975f
pixline <pixline@gmail.com>
2013-10-30 23:39:18 +0100

    more env checks

--- wp-cli-task-command.php
+++ wp-cli-task-command.php
@@ -13,28 +13,97 @@ if ( true === class_exists( 'WP_CLI_Command' ) ){
 
 		private function _check_env(){
 			WP_CLI::log( 'Checking environment..' );
+
+			# check PHP version
+			if (version_compare(phpversion(), '5.4.0', '<')) {
+				WP_CLI::error( 'wp task requires PHP >= 5.4.0 ' );
+			}
 			# check PEAR
 			require_once 'System.php';
 			if ( true === class_exists( 'System' ) ) {
 			   WP_CLI::launch( 'pear version' );
 			} else {
-			   WP_CLI::error( 'Could not find PEAR. Please install/check PEAR and restart setup.' );
+			   WP_CLI::warning( 'Could not find PEAR. Please install it and retry:' );
+			   WP_CLI::warning( 'http://pear.php.net/manual/en/installation.php' );
 			}
-			WP_CLI::launch('$(pwd)/vendor/bin/scrutinizer --version');
-			WP_CLI::launch('$(pwd)/vendor/bin/phpunit --version');
-		}
 
+			$checklist = array(
+				'phpunit' => array(
+					'check' => 'phpunit --version',
+					'url' => 'http://phpunit.de/manual/3.7/en/installation.html'
+				),
+				'scrutinizer' => array(
+					'check' => 'scrutinizer --version',
+					'url' => 'https://github.com/scrutinizer-ci/scrutinizer/'
+				),
+				'phpcs' => array(
+					'check' => 'phpcs --version',
+					'url' => 'https://github.com/squizlabs/PHP_CodeSniffer',
+				),
+				'phploc' => array(
+					'check' => 'phploc --version',
+					'url' => 'https://github.com/sebastianbergmann/phploc',
+				),
+				'phpcpd' => array(
+					'check' => 'phpcpd --version',
+					'url' => 'https://github.com/sebastianbergmann/phpcpd',
+				),
+				'phpcov' => array(
+					'check' => 'phpcov --version',
+					'url' => 'https://github.com/sebastianbergmann/phpcov',
+				),
+				'phpdcd' => array(
+					'check' => 'phpdcd --version',
+					'url' => 'https://github.com/sebastianbergmann/dcd',
+				),
+				'phpdoc' => array(
+					'check' => 'phpdoc --version',
+					'url' => 'http://www.phpdoc.org/docs/latest/for-users/installation.html'
+				),
+				'phpmd' => array(
+					'check' => 'phpmd --version',
+					'url' => 'http://phpmd.org/download/index.html'
+				)
+			);
+
+			foreach ( $checklist as $key => $info ):
+				$test = exec( 'which ' . $key );
+				if ( NULL !== $test ) {
+				   WP_CLI::launch( $info['check'] );
+				} else {
+				   WP_CLI::warning( 'Could not find '.$key.', please install it and retry:' );
+					 WP_CLI::warning( $info['url'] );
+				}
+			endforeach;
+
+			# PHP CodeSniffer WP Coding Standard
+			$pear_config_dir = exec( 'pear config-get php_dir' );
+			$wp_phpcs_path = $pear_config_dir . '/PHP/CodeSniffer/Standards/WordPress';
+			if ( false === is_dir( $wp_phpcs_path ) ):
+				exec( 'git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress' );
+			endif;
+
+			require_once 'PHP/CodeCoverage/Autoload.php';
+			if ( true === class_exists( 'PHP_CodeCoverage' ) ) {
+			   WP_CLI::launch( 'pear version' );
+			} else {
+			   WP_CLI::warning( 'Could not find PHP_CodeCoverage. Please install it and retry:' );
+			   WP_CLI::warning( 'https://github.com/sebastianbergmann/php-code-coverage' );
+			}
+
+
+		}
 
 		/**
 		 * Setup task environment
 		 *
 		 * ## EXAMPLES
 		 *
-		 * wp task setup
+		 * wp task doctor
 		 *
 		 * @since 0.1.0
 		 */
-		public function setup(){
+		public function doctor(){
 			$this->_check_env();
 		}
 	
