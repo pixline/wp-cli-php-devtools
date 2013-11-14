c51ed644e8702b1d9078050726dcd63edb297861
pixline <pixline@gmail.com>
2013-11-14 01:41:20 +0100

    phpmd command

--- commands/phpmd-command.php
+++ commands/phpmd-command.php
@@ -14,7 +14,31 @@ if ( true === class_exists( 'WP_CLI_Command' ) ){
 	 */
 	class WP_CLI_Phpmd_Command extends WP_CLI_Command{
 		
+		/**
+		 * Run PHP Mess Detector
+		 *
+		 * ## OPTIONS
+		 *
+		 * <folder>
+		 * : Folder to check
+		 *
+		 * ## EXAMPLES
+		 *
+		 * wp phpmd run folder/
+		 *
+		 * @synopsis <folder>
+		 *
+		 * @since 0.1.0
+		 */
 		public function run( $args = null, $assoc_args = null ){
+			#print_r($assoc_args);
+			if ( null === $args[0] ):
+				WP_CLI::error( 'Usage: wp phpmd run <folder>' );
+			else:
+				# WP_CLI::launch( 'phpcs -i' );
+				$cmd = 'phpmd ' . $args[0] . ' text codesize,controversial,design,naming,unusedcode --extensions=php';
+				WP_CLI::launch( $cmd );
+			endif;
 		}
 	}
 
